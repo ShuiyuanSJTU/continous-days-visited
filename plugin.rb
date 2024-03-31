c3b45a7a500b2c19aa44af6af8d648f5376114d6
PangBo <51732678+pangbo13@users.noreply.github.com>
2024-03-31 17:09:54 +0800

    feat: clean cache from frontend

--- plugin.rb
+++ plugin.rb
@@ -10,7 +10,7 @@ enabled_site_setting :continous_days_visited_enabled
 
 require_relative 'app/lib/continous_days_visited'
 
-PLUGIN_NAME ||= 'ContinousDaysVisited'
+PLUGIN_NAME ||= 'ContinousDaysVisited'.freeze
 
 after_initialize do
   register_user_custom_field_type "continous_days_visited", :integer
@@ -39,6 +39,47 @@ after_initialize do
   class ::UserSummary
     def continous_days_visited
       ContinousDaysVisited.continous_days_visited(@user)
+    end
+  end
+
+  module ::DiscourseContinousDaysVisited
+    class Engine < ::Rails::Engine
+      engine_name PLUGIN_NAME
+      isolate_namespace ::DiscourseContinousDaysVisited
+    end
+    
+    class ContinousDaysVisitedController < ::ApplicationController
+      def index
+        user = User.find_by(id: params[:user_id])
+        if user.nil?
+          render json: { error: "User not found" }, status: :not_found
+          return
+        end
+        render json: { 
+          user_id: user.id,
+          username: user.username,
+          continous_days_visited: ContinousDaysVisited.continous_days_visited(user) 
+        }
+      end
+      
+      def destroy
+        user = User.find_by(id: params[:user_id])
+        if user.nil?
+          render json: { error: "User not found" }, status: :not_found
+          return
+        end
+        ContinousDaysVisited.clean_stored_continous_days_visited(user)
+        render json: { success: "Cleaned continous days visited" }
+      end
+    end
+
+    Discourse::Application.routes.append { mount Engine, at: "/continous_days_visited" }
+
+    Engine.routes.draw do
+      constraints AdminConstraint.new do
+        get "/:user_id" => "continous_days_visited#index"
+        delete "/:user_id" => "continous_days_visited#destroy"
+      end
     end
   end
 end
